efa7e2bf858800bea37535ca99f28f7b6bb17b3e
India Horton <indy@MacBook-Pro-2.lan>
2022-04-15 14:27:05 -0400

    i am capable

---
+++ JavaCodingChallenge.js
@@ -0,0 +1,7 @@
+let userinput= prompt ("Choose from the following options: 1. Deposit Money or 2. Withdraw Money")
+// //var choice1= "Deposit Money"
+// var choice2= "Withdraw Money"
+// if (choice1 === "How much would you like to deposit?")
+// console.log(choice1)
+// if (choice2 === "How much would you like to withdraw?")
+// console.log(choice2)
